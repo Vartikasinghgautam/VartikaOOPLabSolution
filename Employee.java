769c75c640f617aefdc9a8982ee4bdb193f3c4c4
Vartikasinghgautam <107786347+Vartikasinghgautam@users.noreply.github.com>
2022-06-19 09:16:51 -0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package com.vartikasingh.main;
+
+public class Employee {
+
+	private static final String String = null;
+	private String firstName;
+	private String lastName;
+	
+
+	//constructor to receive the first name and last name
+	public Employee (String firstName,String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}	
+		
+		public String getFirstName() {
+			return firstName;
+			
+		}
+		
+		public String getLastName() {
+			return lastName;
+			
+		}
+		
+
+		
+	
+	}
